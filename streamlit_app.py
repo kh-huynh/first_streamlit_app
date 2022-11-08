3a90c6f84ee84ac74b7cbb408a51a0490e01ed4b
Khanh-Triet HUYNH <117750725+kh-huynh@users.noreply.github.com>
2022-11-08 16:34:14 +0100

    Update streamlit_app.py
    text functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit('menu 1')
-streamlit('menu 2')
-streamlit('menu ')
+streamlit.text('menu 1')
+streamlit.text('menu 2')
+streamlit.text('menu 3')
