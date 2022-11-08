45b71bc654592cf8885c279ae5ea7e5219e134d0
Khanh-Triet HUYNH <117750725+kh-huynh@users.noreply.github.com>
2022-11-08 16:43:22 +0100

    Update streamlit_app.py
    Ajout section smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast Menu')
+
+streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-   
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
