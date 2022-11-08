d6bce5bb968b1d355804ef64f50ccb09b36398b5
Khanh-Triet HUYNH <117750725+kh-huynh@users.noreply.github.com>
2022-11-08 16:53:04 +0100

    Update streamlit_app.py
    Ajout index pour la liste de fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Index mis sur la colonne Fruit
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Favorites')
